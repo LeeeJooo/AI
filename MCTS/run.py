19488a99646f48a0f1bec7f5bef2ec360cf4d437
LeeeJooo <ljhp1004@gmail.com>
2025-04-04 18:07:32 +0900

    update MCTS

--- MCTS/run.py
+++ MCTS/run.py
@@ -8,7 +8,20 @@ def play_game(env, player1, player2):
     end = False
     while not end:
         player1_action = player1.get_action()
-        result = env.step(player1_action)
+        env.step(player1_action)
+        end, winner = env.end_game()
+        if end : break
+        player2_action = player2.get_action()
+        env.step(player2_action)
+        end, winner = env.end_game()
+
+    if winner == PIDS[1]:
+        print('PLAYER1 WINS')
+    if winner == PIDS[2]:
+        print('PLAYER2 WINS')
+    if winner == None:
+        print('DRAW')
+
 
     pass
 
